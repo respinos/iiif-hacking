40a93282b7652954f2f7216dd4f24f0aa47134d7
Roger Espinosa <roger@umich.edu>
2024-03-12 11:41:55 -0400

    navigation + sequences

--- src/js/app6.svelte
+++ src/js/app6.svelte
@@ -11,17 +11,24 @@
   let loaded = false;
   let manifest;
   let canvases;
-  let canvasIdx = 0;
+  let tileSources;
+  let ranges;
+  let canvasIdx = 1;
+  let lastCanvasIdx;
 
   let dragonEl;
   let dragon;
+
+  let detailsGroupEl;
 
   let btnZoomIn;
   let btnZoomOut;
   let btnHome;
   let btnFullPage;
+  let btnNextCanvas;
+  let btnPreviousCanvas;
 
-  let imageToolbarOpen = true;
+  let imageToolbarOpen = false;
   let fx = {
     brightness: 100,
     contrast: 100,
@@ -74,6 +81,33 @@
     let imageId = image.getResource().getServices()[0].id;
     return `${imageId}/info.json`;
   }
+
+  function openCanvas(canvas, idx) {
+    canvasIdx = idx + 1;
+    dragon.goToPage(idx);
+  }
+
+  function gotoCanvasId(canvasId) {
+    console.log("-- gotoCanvasId", canvasId);
+    const canvas = canvases.find((c) => c.id == canvasId);
+    const idx = canvases.findIndex((c) => c.id == canvasId);
+    openCanvas(canvas, idx);
+  }
+
+  function togglePanels(event) {
+    if (event.target.localName === 'sl-details') {
+      [...detailsGroupEl.querySelectorAll('sl-details')].map(details => (details.open = event.target === details));
+    }
+  }
+
+  function onCanvasChange(event) {
+    console.log("-- onCanvasChange", canvasIdx, lastCanvasIdx);
+    if ( canvasIdx <= 0 ) { canvasIdx = lastCanvasIdx; }
+    else if ( canvasIdx > canvases.length ) { canvasIdx = lastCanvasIdx; }
+    else {
+      dragon.goToPage(canvasIdx - 1);
+    }
+  }
   
   // $: manifesto.loadManifest(manifestUrl).then((data) => {
   //   manifest = manifesto.parseManifest(data);
@@ -84,13 +118,22 @@
     setTimeout(() => {
     manifesto.loadManifest(manifestUrl).then((data) => {
       manifest = manifesto.parseManifest(data);
+      window.manifest = manifest;
       canvases = manifest.getSequences()[0].getCanvases();
+      tileSources = [];
+      canvases.forEach((canvas) => {
+        tileSources.push(getInfoUrl(canvas));
+      })
+      let range = manifest.getTopRanges();
+      if ( range.length ) {
+        ranges = range[0].getRanges();
+      }
       loaded = true;
     });
     }, 0)
   }
 
-  $: if ( loaded && dragonEl !== undefined ) {
+  $: if ( loaded && dragonEl !== undefined && dragon === undefined ) {
     // setTimeout(() => {
       dragon = OpenSeadragon({
         element: dragonEl,
@@ -109,8 +152,18 @@
         zoomInButton: btnZoomIn,
         zoomOutButton: btnZoomOut,
         homeButton: btnHome,
+        nextButton: btnNextCanvas,
+        previousButton: btnPreviousCanvas,
+        sequenceMode: true,
+        tileSources: tileSources,
+        initialPage: canvasIdx - 1,
       });
-      dragon.open(getInfoUrl(canvases[canvasIdx]));
+
+      dragon.addHandler('page', (event) => {
+        console.log('-- page', event);
+        canvasIdx = event.page + 1;
+      })
+
       window.dragon = dragon;
     // }, 100);
     console.log("AHOY", loaded, dragonEl, getInfoUrl(canvases[canvasIdx]));
@@ -125,28 +178,91 @@
 
 <PaneGroup direction="horizontal">
   <Pane defaultSize={25} class="viewer--sidebar">
-    <pre>{message}</pre>
-    {#if loaded}
-    <pre>Loaded: {canvases.length}</pre>
-    <div class="thumbnail-list">
-      <ul class="list-unstyled flex flex-flow-column flex-align-center">
-        {#each canvases as canvas (canvas.id)}
-          {@const image = canvas.getImages()[0]}
-          {@const imageId = image.getResource().getServices()[0].id}
-          <li class="w-80">
-            <figure class="m-0 w-100">
-              <button 
-                on:click={dragon.open(getInfoUrl(canvas))} 
-                type="button" 
-                class="button button--ghost flex flex-flow-column w-100 flex-align-center flex-justify-center gap-0_125 p-half"
-                >
-                <img loading="lazy" src="{imageId}/full/!150,150/0/default.jpg" alt="" class="border" />
-                <p class="text-xxx-small m-0">{canvas.getLabel().getValue()}</p>
-              </button>
-            </figure>
-          </li>
-        {/each}
-      </ul>
+    <div style="max-height: 100%; padding: 1rem; display: grid; grid-template-rows: minmax(0, 1fr);">
+    <sl-tab-group>
+      <!-- <sl-tab slot="nav" panel="message">Message</sl-tab> -->
+      {#if loaded}
+        <sl-tab slot="nav" panel="items">Pages</sl-tab>
+        {#if ranges}
+        <sl-tab slot="nav" panel="ranges">Index</sl-tab>
+        {/if}
+      {/if}
+
+      <sl-tab-panel name="message">
+        <pre>{message}</pre>
+      </sl-tab-panel>
+      {#if loaded}
+        <sl-tab-panel name="items">
+          <div style="height: 100%; overflow: auto;">
+          <ul class="list-unstyled flex flex-flow-column flex-align-center">
+            {#each canvases as canvas, idx (canvas.id)}
+              {@const image = canvas.getImages()[0]}
+              {@const imageId = image.getResource().getServices()[0].id}
+              <li class="w-80">
+                <figure class="m-0 w-100">
+                  <button 
+                    on:click={openCanvas(canvas, idx)} 
+                    type="button" 
+                    class="button button--ghost flex flex-flow-column w-100 flex-align-center flex-justify-center gap-0_125 p-half"
+                    >
+                    <img loading="lazy" src="{imageId}/full/!150,150/0/default.jpg" alt="" class="border" />
+                    <p class="text-xxx-small m-0">{canvas.getLabel().getValue()}</p>
+                  </button>
+                </figure>
+              </li>
+            {/each}
+          </ul>
+          </div>          
+        </sl-tab-panel>
+          {#if ranges}
+          <sl-tab-panel name="ranges">
+            <ul>
+              {#each ranges as range}
+                {@const label = range.getLabel().getValue()}
+                {@const canvasId = range.getCanvasIds()[0]}
+                <li>
+                  <a href="#" on:click={gotoCanvasId(canvasId)}>{label}</a></li>
+              {/each}
+            </ul>
+          </sl-tab-panel>
+          {/if}
+        {/if}
+    </sl-tab-group>
+    </div>
+    {#if false}
+    <div class="details-group" on:sl-show={togglePanels} bind:this={detailsGroupEl}>
+      <sl-details summary="Message" open>
+        <pre>{message}</pre>
+      </sl-details>
+      {#if loaded}
+        {#if ranges}
+          <sl-details summary="Index">
+            <pre>THERE WOULD BE CONTENTS</pre>
+          </sl-details>
+        {/if}
+        <sl-details summary="Items">
+          <div style="height: 100%; overflow: auto;">
+          <ul class="list-unstyled flex flex-flow-column flex-align-center">
+            {#each canvases as canvas, idx (canvas.id)}
+              {@const image = canvas.getImages()[0]}
+              {@const imageId = image.getResource().getServices()[0].id}
+              <li class="w-80">
+                <figure class="m-0 w-100">
+                  <button 
+                    on:click={openCanvas(canvas, idx)} 
+                    type="button" 
+                    class="button button--ghost flex flex-flow-column w-100 flex-align-center flex-justify-center gap-0_125 p-half"
+                    >
+                    <img loading="lazy" src="{imageId}/full/!150,150/0/default.jpg" alt="" class="border" />
+                    <p class="text-xxx-small m-0">{canvas.getLabel().getValue()}</p>
+                  </button>
+                </figure>
+              </li>
+            {/each}
+          </ul>
+          </div>
+        </sl-details>
+      {/if}
     </div>
     {/if}
   </Pane>
@@ -179,7 +295,7 @@
                   <span class="material-icons">brightness_5</span>
                 </button>
                 <div class="image-options" class:hidden={!fxToolsOpen.brightness}>
-                  <input type="range" orient="veritcal" min="0" max="200" bind:value={fx.brightness} on:change={(evt) => updateFx({ brightness: evt.target.value })} />
+                  <input type="range" orient="vertical" min="0" max="200" bind:value={fx.brightness} on:change={(evt) => updateFx({ brightness: evt.target.value })} />
                 </div>
               </div>
               <div style="position: relative">
@@ -189,7 +305,7 @@
                   <span class="material-icons">exposure</span>
                 </button>
                 <div class="image-options" class:hidden={!fxToolsOpen.contrast}>
-                  <input type="range" orient="veritcal" min="0" max="200" bind:value={fx.contrast} on:change={(evt) => updateFx({ contrast: evt.target.value })} />
+                  <input type="range" orient="vertical" min="0" max="200" bind:value={fx.contrast} on:change={(evt) => updateFx({ contrast: evt.target.value })} />
                 </div>
               </div>
               <div style="position: relative">
@@ -199,7 +315,7 @@
                   <span class="material-icons">gradient</span>
                 </button>
                 <div class="image-options" class:hidden={!fxToolsOpen.saturate}>
-                  <input type="range" orient="veritcal" min="0" max="200" bind:value={fx.saturate} on:change={(evt) => updateFx({ saturate: evt.target.value })} />
+                  <input type="range" orient="vertical" min="0" max="200" bind:value={fx.saturate} on:change={(evt) => updateFx({ saturate: evt.target.value })} />
                 </div>
               </div>
               <button class="button button--square button--ghost" 
@@ -241,13 +357,13 @@
           <div style="width: 1px; height: 50%; background: #999; margin: 0 0.25rem;"></div>
           <div class="flex flex-flow-row flex-align-center" style="gap: 0.125rem; font-size: 0.875rem;">
             <label for="jumpToSeq" class="col-form-label">#</label>
-            <input class="form-control" id="jumpToSeq" type="text" style="width: 4ch; text-align: center;" bind:value={canvasIdx} />
+            <input class="form-control" id="jumpToSeq" type="text" style="width: 4ch; text-align: center;" bind:value={canvasIdx} on:focus={() => lastCanvasIdx = canvasIdx} on:change={onCanvasChange} />
             <span style="text-wrap: none;"> / {canvases.length}</span>
           </div>
-          <button type="button" class="button button--ghost button--square">
+          <button type="button" class="button button--ghost button--square" bind:this={btnPreviousCanvas}>
             <span class="material-icons" style="transform: rotate(-90deg);">arrow_circle_up</span>
           </button>
-          <button type="button" class="button button--ghost button--square">
+          <button type="button" class="button button--ghost button--square" bind:this={btnNextCanvas}>
             <span class="material-icons" style="transform: rotate(90deg);">arrow_circle_up</span>
           </button>
         </div>
@@ -260,14 +376,37 @@
 
 <style>
 
-  .thumbnail-list {
+  /* .thumbnail-list {
     overflow: auto;
+  } */
+
+  .details-group {
+    overflow: hidden;
+  }
+
+  sl-details {
+    max-height: 100%;
+  }
+
+  sl-tab-group sl-tab {
+    margin-top: 4px;
+  }
+
+  sl-tab-group sl-tab:first-child {
+    margin-left: 4px;
+  }
+
+  sl-tab-group::part(base) {
+    display: grid !important;
+    grid-template-rows: min-content 1fr;
+    max-height: 100%;
+    overflow: hidden;
   }
 
   .image-viewer-pane {
     display: grid;
     grid-template-rows: 1fr min-content;
-    gap: 0.5rem;
+    /* gap: 0.5rem; */
     /* padding-bottom: 0.125rem; */
 
     height: 100%;
@@ -279,6 +418,7 @@
   .image-viewer-wrap {
     background: inherit;
     position: relative;
+    max-height: 100%;
   }
 
   .image-viewer-toolbar {
@@ -372,12 +512,12 @@
     display: none !important;
   }
 
-  input[type="range"] {
+  /* input[type="range"] {
     appearance: slider-vertical;
     width: 1rem;
     height: 10rem;
     padding: 0 0.125rem;
     flex-grow: 1;
-  }
+  } */
 
 </style>
