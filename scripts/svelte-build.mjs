2163e9221334870b9bf9cf28fdab88ccb958c98a
Roger Espinosa <roger@umich.edu>
2024-03-14 09:35:11 -0400

    fix build script

--- scripts/svelte-build.mjs
+++ scripts/svelte-build.mjs
@@ -3,13 +3,12 @@ import sveltePlugin from "esbuild-svelte";
 
 esbuild
   .build({
-    entryPoints: ["src/js/app5.js"],
+    entryPoints: ["src/js/app7.js"],
     mainFields: ["svelte", "browser", "module", "main"],
     conditions: ["svelte", "browser"],
     bundle: true,
-    outfile: "dist/js/app5.js",
-    plugins: [sveltePlugin()],
+    outfile: "dist/js/app7.js",
+    plugins: [sveltePlugin({compilerOptions: { css: true }})],
     logLevel: "info",
-    watch: true,
   })
   .catch(() => process.exit(1));
